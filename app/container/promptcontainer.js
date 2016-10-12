a82bb4ef40c1e4ec7597a2e1934ad711bf42b84c
Maia Stone <maiadov@gmail.com>
2016-10-12 11:14:55 -0600

    refactor LocationSearch to es6

--- app/container/promptcontainer.js
+++ app/container/promptcontainer.js
@@ -1,23 +1,40 @@
 var React = require('react');
 
 var PromptContainer = React.createClass({
+
   render: function() {
     return (
-    <div className="location">
-      <h1>Enter your location</h1>
-              <div>
-                    <input
-                    placeholder="location"
-                    type="text" />
-
-                    <button className="submit-button"
-                    type="submit">Get Weather
-                    </button>
-
-              </div>
+    <div aria-role="search" className="location">
+      <LocationSearch />
     </div>
   )
   }
 });
+
+class LocationSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = {location: ''};
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Enter your location</h1>
+        <div>
+          <input
+          placeholder="location" aria-label="search location"
+          value={this.state.location}
+          onChange={(e)=> {this.setState({location: e.target.value})}}
+          type="text" />
+
+          <button className="submit-button"
+          type="submit">Get Weather
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
 module.exports = PromptContainer;
