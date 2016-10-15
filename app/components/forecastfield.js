f84dc012e979a0480cfddef464aca0482704417c
Alex Pilewski <pilewski.alex@gmail.com>
2016-10-15 16:38:50 -0600

    converted date

--- app/components/forecastfield.js
+++ app/components/forecastfield.js
@@ -1,23 +1,39 @@
+/*jshint esversion: 6 */
 const React = require('react');
 const Weekday = require('./weekdays');
 
-
 class ForecastField extends React.Component {
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
 
   getDayInfo(data) {
+    var Month = {
+      '01' : 'January',
+      '02' : 'February',
+      '03' : 'March',
+      '04' : 'April',
+      '05' : 'May',
+      '06' : 'June',
+      '07' : 'July',
+      '08' : 'August',
+      '09' : 'September',
+      '10' : 'October',
+      '11' : 'November',
+      '12' : 'December'
+    };
+    let date = data.date;
+    let convertedDate = Month[date.slice(0,2)] + ' ' + date.slice(3,5) + 'th';
+
     return (
       <ul className={data.weatherType.type}>
-        <li>{data.date}</li>
-        <li>{data.weatherType.type}</li>
+        <li> { convertedDate } </li>
+        <li> {data.weatherType.type} </li>
         <li>High: {data.temp.high}</li>
         <li>Low: {data.temp.low}</li>
       </ul>
     )
   }
-
 
   render() {
     return (
