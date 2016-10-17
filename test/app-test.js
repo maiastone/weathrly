ac84517ab95ae199185dbd04ac7c47bf105c1545
Alex Pilewski <pilewski.alex@gmail.com>
2016-10-17 16:48:16 -0600

    test suite functional

--- test/app-test.js
+++ test/app-test.js
@@ -1,27 +1,23 @@
-var expect = require('chai').expect
-var assert = require('chai').assert
+/*jshint esversion: 6 */
+
+var expect = require('chai').expect;
+var assert = require('chai').assert;
 const React = require('react');
 const $ = require('jquery');
 const SubmitButton = require('../app/components/submitbutton');
 const ForecastField = require('../app/components/forecastfield');
 const LocationSearch = require('../app/components/locationsearch');
 const App = require('../app/container/app');
+require('locus')
 import { shallow, mount, render } from 'enzyme';
 
 
 describe('app.js', function(){
   it('renders an input field on the page', function() {
+    //define which wrapper i want to use
+    const wrapper = mount (<App/>);
+    wrapper.find('#city').simulate('change', {target: {value: 'denver'} });
+    expect(wrapper.state('location')).to.equal('denver');
+  });
 
-
-    shallow(<App />).contains(<LocationSearch />).to.be.true;
-
-
-    // expect(browser.find('#app-header').to.have.length(1));
-  })
-
-  // it('renders a submit button', function(){
-  //   const wrapper = render(<App />)
-  //   expect(wrapper.contains(<SubmitButton handleClick={() => this.ajaxRequest()}/>).to.be.true
-  // })
-
-})
+});
