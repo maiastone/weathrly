7192d9b8ae255afeb65982244e00aa9b73506ab6
Maia Stone <maiadov@gmail.com>
2017-01-22 15:19:22 -0700

    Fix import of React

--- app/components/forecastfield.js
+++ app/components/forecastfield.js
@@ -1,4 +1,4 @@
-const React = require('react');
+import React from 'react';
 const Weekday = require('./weekdays');
 
 class ForecastField extends React.Component {
--- app/components/weekdays.js
+++ app/components/weekdays.js
@@ -1,4 +1,4 @@
-const React = require('react');
+import React from 'react';
 
 class Weekday extends React.Component {
 
